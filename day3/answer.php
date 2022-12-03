d3119aeddb0dedc868b8d7488cb36f588e9f226f
Son Do <son@chillbits.com>
2022-12-03 05:50:18 +0000

    OK ne

--- day3/answer.php
+++ day3/answer.php
@@ -3,15 +3,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $rucksacks = collect(include __DIR__ . '/stubs/input.php');
 
-//$part1 = $rucksacks->map(function($data){
-//    return (new Rucksack($data))->toArray();
-//});
-//
-//dd($part1->sum('priority'));
+$part1 = $rucksacks->map(function ($data) {
+    return (new Rucksack($data))->toArray();
+});
+
+echo $part1->sum('priority');
+
 $part2 = collect();
 
 $rucksacks->chunk(3)->each(function ($chunk) use (&$part2) {
     $part2->push((new Group($chunk->values()))->toArray());
 });
 
-dd($part2->sum('priority'));
+echo $part2->sum('priority');
